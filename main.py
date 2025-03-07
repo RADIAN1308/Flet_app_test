73f2ef45a1c755554d7cd96a22d9d2fc21206c59
Adrian Brine <adrian.brine135@gmail.com>
2025-03-08 01:48:20 +0530

    gradient added

--- main.py
+++ main.py
@@ -4,6 +4,25 @@ from flet import *
 
 #color gradient function
 
+def create_darkness():
+    gradient = LinearGradient(
+        begin= alignment.top_center,
+        end=alignment.bottom_center,
+        colors=[
+            "#484848",
+            "#444444",
+            "#424242",
+            "#3b3b3b",
+            "#373737",
+            "#353535",
+            "#343434",
+            "#2f2f2f",
+            "#2b2b2b",
+        ]
+    )
+    return gradient
+
+
 
 
 
@@ -13,7 +32,7 @@ def main(page: Page):
     # page settings
     page.horizontal_alignment = "center"
     page.vertical_alignment = "center"
-    page.bgcolor= "PINK"
+    page.bgcolor= "#ffffff"
 
     #create instance
 
